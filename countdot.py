12fa078b290a6e08c3130661aec8d3a3a9e9d25f
Ayush <ayush0477@gmail.com>
2020-01-18 00:00:24 +0530

    Add files via upload

--- countdot.py
+++ countdot.py
@@ -2,8 +2,8 @@ a= str(input("enter the string\n"))
 a = a.lower()
 count = 0
 read = 0
-for count in range(len(a)):
-   if a[count]=="c" or a[count]=="o" or a[count]=="e":
+for count in range(len(a)-3):
+   if a[count]=="c" and a[count+1]=="o" and a[count+2]=="d" and a[count+3]=="e":
         count=count+1
         read= read+1
 print(read)
