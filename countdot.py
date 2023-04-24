00799ee295d22ef8072b7435d684d514c8de6a9b
ayush0477 <ayush0477@gmail.com>
2023-04-24 15:26:42 +0530

    Update countdot.py

--- countdot.py
+++ countdot.py
@@ -6,4 +6,4 @@ for count in range(len(a)-3):
    if a[count]=="c" and a[count+1]=="o" and a[count+2]=="d" and a[count+3]=="e":
         count=count+1
         read= read+1
-print(read)
+print(read)#hello
